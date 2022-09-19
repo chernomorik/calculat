5f3707efa92d046db349606bf0378b210f8f1f48
chernomorik <113508781+chernomorik@users.noreply.github.com>
2022-09-19 16:52:38 +0500

    Second part

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,23 +1,37 @@
 fun main(){
     var result:Int = 0
     println("Введите первое число")
-    var a:Int = readLine()!!.toInt()
+    var a:Double = readLine()!!.toDouble()
     println("Введите второе число")
-    var b:Int = readLine()!!.toInt()
+    var b:Double = readLine()!!.toDouble()
     println("Выберите операцию")
     println("1 - сумма")
     println("2 - разность")
+    println("3 - произведение")
+    println("4 - частность")
     var choose:Int = readLine()!!.toInt()
     if (choose==1)
     {
-        result = a + b
+        result = (a + b).toInt()
         println("Сумма равна:")
         println(result)
     }
     if (choose==2)
     {
-        result = a - b
+        result = (a - b).toInt()
         println("Разность равна:")
+        println(result)
+    }
+    if (choose==3)
+    {
+        result = (a * b).toInt()
+        println(" Произведение равно:")
+        println(result)
+    }
+    if (choose==4)
+    {
+        result = (a / b).toInt()
+        println("Частность равна:")
         println(result)
     }
 }
